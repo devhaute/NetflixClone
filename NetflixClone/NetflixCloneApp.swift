e14949535d34f50d30087b5d669073d93f115df2
devhaute <devhaute@gmail.com>
2021-11-30 19:31:03 +0900

    초기 환경 세팅

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by devhaute on 2021/11/30.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
